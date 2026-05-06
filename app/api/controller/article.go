be72dd81d4919fb8997bab58b8dc12e5343b43a6
zhu885744 <2776686748@qq.com>
2026-05-06 12:24:07 +0800

    去除了文章发布接口和独立页面发布接口中的 XSS 防护代码

--- app/api/controller/article.go
+++ app/api/controller/article.go
@@ -338,14 +338,6 @@ func (this *Article) create(ctx *gin.Context) {
 			case "slice":
 				val = strings.Join(cast.ToStringSlice(val), ",")
 			case "string":
-				// 检测是否包含XSS攻击
-				if key == "title" || key == "abstract" || key == "content" || key == "remark" || key == "text" {
-					if facade.Comm.DetectXSS(cast.ToString(val)) {
-						this.json(ctx, nil, facade.Lang(ctx, "内容包含恶意代码，禁止提交！"), 400)
-						return
-					}
-					val = facade.Comm.SanitizeHTML(cast.ToString(val))
-				}
 			}
 			utils.Struct.Set(&table, key, val)
 		}
@@ -417,14 +409,6 @@ func (this *Article) update(ctx *gin.Context) {
 			case "slice":
 				val = strings.Join(cast.ToStringSlice(val), ",")
 			case "string":
-				// 检测是否包含XSS攻击
-				if key == "title" || key == "abstract" || key == "content" || key == "remark" || key == "text" {
-					if facade.Comm.DetectXSS(cast.ToString(val)) {
-						this.json(ctx, nil, facade.Lang(ctx, "内容包含恶意代码，禁止提交！"), 400)
-						return
-					}
-					val = facade.Comm.SanitizeHTML(cast.ToString(val))
-				}
 			}
 			async.Set(key, val)
 		}
